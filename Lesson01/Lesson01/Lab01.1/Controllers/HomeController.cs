ba344c4467674e5bb6ee33b1cc81cc05ab42236e
vutuan394 <138289477+vutuan394@users.noreply.github.com>
2024-05-22 13:40:00 +0700

    Leson01

---
+++ Lesson01/Lesson01/Lab01.1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Sử dụng ViewBag để truyền dữ liệu vào View
+            ViewBag.message="Chào Mừng Bạn Đến Với ASP.NET MVC 5";  
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.message = "Đây là Trang About";
+            return View();
+        }
+    }
+}
---
+++ Lesson01/Lesson01/Lab01.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab01._1.MvcApplication" Language="C#" %>
